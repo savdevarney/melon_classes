b04235fea707a23e27f82a163d6da3831ba6529b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-01 12:49:39 -0700

    updated functions to handle date/time

--- melons.py
+++ melons.py
@@ -1,8 +1,10 @@
 """Classes for melon orders."""
 
+from random import randint
+import datetime
+
 
 class AbstractMelonOrder(object):
-
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
@@ -11,10 +13,22 @@ class AbstractMelonOrder(object):
         self.qty = qty
         self.shipped = False
 
+    def get_base_price(self):
+        base_price = randint(5, 9)
+        #add $4 charge if day is mon-fri and 8am-11am
+        current_datetime = datetime.datetime.now()
+        day = current_datetime.weekday()
+        time = current_datetime.hour
+        #time = 10 <-- to test
+        if day in range(5) and time in range(8, 12):
+            base_price += 4.00
+        return base_price
+
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = self.get_base_price()
+
         if self.species == "Christmas":
             base_price = 1.5 * base_price
 
@@ -55,3 +69,19 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A government melon order."""
+
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes"""
+        super(GovernmentMelonOrder, self).__init__(species, qty)
+        self.order_type = "domestic"
+        self.tax = 0.00
+        self.passed_inspection = False
+
+
+    def mark_inspection(self, passed):
+
+        self.passed_inspection = passed
